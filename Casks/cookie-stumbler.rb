ea3ff0b8b6fe96a521f38a92565a718dd0144aa6
coeligena <coeligenai@gmail.com>
2014-11-16 21:43:27 +0200

    Create cookie-stumbler.rb

---
+++ Casks/cookie-stumbler.rb
@@ -0,0 +1,11 @@
+class CookieStumbler < Cask
+  version :latest
+  sha256 :no_check
+  
+  url 'https://cookiestumbler.s3.amazonaws.com/Cookie%20Stumbler.dmg'
+  appcast 'http://www.cookiestumbler.com/Software%20Updates/Cookie%20Stumbler.xml'
+  homepage 'http://www.writeitstudios.com/cookiestumbler.php'
+  license :trial
+  
+  app 'Cookie Stumbler.app'
+end
