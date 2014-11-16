2734387a6e90cd9c56f208d3381ebfc59fdf0632
coeligena <coeligenai@gmail.com>
2014-11-16 19:07:48 +0200

    Create cookie.rb

---
+++ Casks/cookie.rb
@@ -0,0 +1,11 @@
+class Cookie < Cask
+  version :latest
+  sha256 :no_check
+  
+  url 'https://sweetpproductions.com/products/cookie/Cookie.dmg'
+  appcast 'http://www.cookiestumbler.com/Software%20Updates/Cookie%20Stumbler.xml'
+  homepage 'http://cookieapp.com'
+  license :trial
+  
+  app 'Cookie.app'
+end
