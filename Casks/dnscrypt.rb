66ee51533d3ad6737d969db0f869290ed5533224
coeligena <coeligenai@gmail.com>
2014-11-16 21:45:01 +0200

    Update dnscrypt.rb

--- Casks/dnscrypt.rb
+++ Casks/dnscrypt.rb
@@ -1,18 +1,18 @@
 cask :v1 => 'dnscrypt' do
-  version '1.0.3'
-  sha256 '08fc0ee0a029a1a99ffc44a0cb5f7f844f41e7fb0a30e31fea714ac103d69557'
-
+  version '1.0.4'
+  sha256 :no_check
+  
   url "https://github.com/alterstep/dnscrypt-osxclient/releases/download/#{version}/dnscrypt-osxclient-#{version}.dmg"
-  homepage 'http://opendns.github.io/dnscrypt-osx-client/'
+  homepage 'https://opendns.github.io/dnscrypt-osx-client/'
   license :oss
-
+  
   pkg 'DNSCrypt.mpkg'
-  uninstall :quit      => 'com.github.dnscrypt-osxclient.DNSCrypt-Menubar',
-            :pkgutil   => 'com.github.dnscrypt-osxclient.dnscryptClient.*',
-            :launchctl => [
-                           'com.github.dnscrypt-osxclient.DNSCryptAfterboot',
-                           'com.github.dnscrypt-osxclient.DNSCryptConsoleChange',
-                           'com.github.dnscrypt-osxclient.DNSCryptControlChange',
-                           'com.github.dnscrypt-osxclient.DNSCryptNetworkChange',
-                          ]
+  uninstall :quit => 'com.github.dnscrypt-osxclient.DNSCrypt-Menubar',
+      :pkgutil    => 'com.github.dnscrypt-osxclient.dnscryptClient.*',
+      :launchctl  => [
+    'com.github.dnscrypt-osxclient.DNSCryptAfterboot',
+    'com.github.dnscrypt-osxclient.DNSCryptConsoleChange',
+    'com.github.dnscrypt-osxclient.DNSCryptControlChange',
+    'com.github.dnscrypt-osxclient.DNSCryptNetworkChange',
+  ]
 end
