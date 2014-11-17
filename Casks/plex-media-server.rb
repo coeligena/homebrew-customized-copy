697614463bd9dc48d24345bedc4e7789886e6381
coeligena <coeligenai@gmail.com>
2014-11-17 22:42:00 +0200

    Update plex-media-server.rb

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'plex-media-server' do
-  version '0.9.9.14.531-7eef8c6'
+  version '0.9.11.1.678-c48ffd2'
   sha256 '7cd91e89f74569409b12f8151d66c731ca8658e6df61aaaac41accf6e6cba337'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
