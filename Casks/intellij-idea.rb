aed43dfe3fddf80c635c6fee36ffa31b1eda60cc
coeligena <coeligenai@gmail.com>
2014-11-17 04:16:55 +0200

    Update intellij-idea.rb

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'intellij-idea' do
-  version '14'
-  sha256 '9695bedfe145d70010b853cc67d7dc07bf2768e38b5b9034d1f6a00b0b9a15a7'
+  version '14.0.1'
+  sha256 :no_check
 
   url "http://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   homepage 'https://www.jetbrains.com/idea/index.html'
