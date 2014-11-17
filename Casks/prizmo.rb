ca7415cdc41a5ae87745defa0e4aa067005b5d42
coeligena <coeligenai@gmail.com>
2014-11-18 00:48:52 +0200

    Update prizmo.rb

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,11 +1,11 @@
-cask :v1 => 'prizmo' do
-  version :latest
-  sha256 :no_check
-
-  url 'http://www.creaceed.com/downloads/prizmo2.zip'
-  appcast 'http://www.creaceed.com/appcasts/prizmo2.xml'
+class Prizmo < Cask
+  version '3'
+  sha256 '309ee4e6d19ad8bfb03dd990909d82515e08b778bb3ac09c2736254fb8259bf4'
+  
+  url 'http://www.creaceed.com/downloads/prizmo3.zip'
+  appcast 'http://www.creaceed.com/appcasts/prizmo3.xml'
   homepage 'http://www.creaceed.com/prizmo'
-  license :unknown
-
+  license :trial
+  
   app 'Prizmo.app'
 end
