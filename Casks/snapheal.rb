6b61103cc9ad5f5034cb84751cc8eadc3f00c1e6
coeligena <coeligenai@gmail.com>
2014-11-19 01:22:05 +0200

    Update snapheal.rb

--- Casks/snapheal.rb
+++ Casks/snapheal.rb
@@ -2,7 +2,7 @@ class Snapheal < Cask
   version '2.5'
   sha256 'd600c7679931065243c11b1d6fdb2a498be4ae1f904bf0b2a256b676796ac4c7'
   
-  url 'https://s3-eu-west-1.amazonaws.com/snapheal/SnaphealNA_v#{version.gsub('.','_')}.dmg'
+  url "https://s3-eu-west-1.amazonaws.com/snapheal/SnaphealNA_v#{version.gsub('.','_')}.dmg"
   appcast 'https://s3-eu-west-1.amazonaws.com/snapheal/'
   homepage 'http://macphun.com/eu/snapheal'
   license :trial
