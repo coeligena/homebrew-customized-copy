7f533ed2f9ff9fc3ac39eba7ee725a7da0b9b8af
coeligena <coeligenai@gmail.com>
2014-11-19 00:24:19 +0200

    Update hive.rb

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,13 +1,12 @@
-cask :v1 => 'hive' do
-  version '1.3.4'
-  sha256 'bbf99210f9d359d1f75482abebd24eb7cb3103d437c47a0dbf107f4bf9dcd97b'
-
-  url "https://github.com/hivewallet/hive-osx/releases/download/#{version}/Hive-#{version}.zip"
+class Hive < Cask
+  version '1.4.1'
+  sha256 '380f056ee5f88c86e69f1f08723db341aa482f36eb048972664d661f44efa58b'
+  
+  url 'https://github.com/hivewallet/hive-osx/releases/download/#{version}/Hive-#{version}.zip'
   appcast 'https://hivewallet.com/hive-osx-appcast.xml',
-          :sha256 => 'ab99fa8762d778ae379837c083214d70b3693b42cc8aa596d57a90b1d7e55c67'
-  homepage 'http://www.hivewallet.com'
+  homepage 'https://www.hivewallet.com'
   license :oss
-
+  
   app 'Hive.app'
 
   postflight do
