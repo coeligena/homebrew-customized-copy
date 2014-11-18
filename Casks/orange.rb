645fe620316c3fa6e5a29fb0218f80097770a23f
coeligena <coeligenai@gmail.com>
2014-11-18 03:07:41 +0200

    Update orange.rb

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -2,7 +2,7 @@ cask :v1 => 'orange' do
   version '2.7.8-249a5a5'
   sha256 'afaa9f22c6950ba653f6122dbd00b3f542887a4985b695f8d9cbb53698999133'
 
-  url "http://orange.biolab.si/download/Orange-#{version}.dmg"
+  url "http://orange.biolab.si/download/files/Orange-#{version}.dmg"
   homepage 'http://orange.biolab.si/'
   license :gpl
 
