3bea68681c0c79affd31d89144f0927f2275332b
coeligena <coeligenai@gmail.com>
2014-11-18 22:38:06 +0200

    Create kismac2.rb

---
+++ Casks/kismac2.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'kismac2' do
+  version :latest
+  sha256 :no_check
+
+  url "http://www.igrsoft.com/wp-content/plugins/download-monitor/download.php?id=58"
+  homepage 'http://www.igrsoft.com/en/kismac2/'
+  license :oss
+
+  app 'KisMac2.app'
+end
