39bcfe7b1c91e5d0dee6f528e189fe946920ca31
coeligena <coeligenai@gmail.com>
2014-11-18 23:24:24 +0200

    Create igadget-manager.rb

---
+++ Casks/igadget-manager.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'igadget-manager' do
+  version :latest
+  sha256 :no_check
+
+  url "http://www.igrsoft.com/wp-content/plugins/download-monitor/download.php?id=56"
+  homepage 'http://www.igrsoft.com/en/igadget-manager/'
+  license :gratis
+
+  preflight do
+    system '/bin/mv', '--', staged_path.join('igadget-manager-latest.php'), destination_path.join('igadget-manager-latest.zip')
+  end
+  app 'iGadget Manager.app'
+end
