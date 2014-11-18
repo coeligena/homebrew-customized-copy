ae15f250a6f09fd941ed6adb6c3557cb5027e606
coeligena <coeligenai@gmail.com>
2014-11-19 00:50:28 +0200

    Create decompose.rb

---
+++ Casks/decompose.rb
@@ -0,0 +1,10 @@
+class Decompose < Cask
+  version '1.2.1'
+  sha256 '68d4dcc0d8969d9b198bb7bdb32808dc2b90d6eb92ccf8d454c141b069b703d9'
+  
+  url "https://www.oneperiodic.com/files/Decompose%20v#{version}.zip"
+  homepage 'https://www.oneperiodic.com/products/decompose/'
+  license :trial
+  
+  app 'Decompose.app'
+end
