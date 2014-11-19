51900cfc5114a5b3de9ad9a188e26c9ebbdeeb61
coeligena <coeligenai@gmail.com>
2014-11-19 02:10:10 +0200

    Create horst.rb

---
+++ Casks/horst.rb
@@ -0,0 +1,10 @@
+class Horst < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://penck.de/download/horst/zip/latest'
+  homepage 'http://penck.de/horst/'
+  license :trial
+
+  app 'Horst.app'
+end
