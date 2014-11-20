a17fcebfa20ff34e1f5ad946cc5b9748aaab12ae
coeligena <coeligenai@gmail.com>
2014-11-21 01:02:50 +0200

    Create 2do.rb

---
+++ Casks/2do.rb
@@ -0,0 +1,10 @@
+cask :v1 => '2do' do
+  version '1.5.2'
+  sha256 :no_check
+
+  url 'http://2doapp.com/macfiles/2Do.26100.dmg'
+  homepage 'http://www.2doapp.com/mac/'
+  license :trial
+
+  app '2Do.app'
+end
