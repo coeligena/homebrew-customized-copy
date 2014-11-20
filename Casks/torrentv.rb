18789588ccef6ac1bb3bbbe03f40de7de8879345
coeligena <coeligenai@gmail.com>
2014-11-20 20:14:20 +0200

    Update torrentv.rb

--- Casks/torrentv.rb
+++ Casks/torrentv.rb
@@ -1,6 +1,6 @@
 class Torrentv < Cask
   version '0.9.6'
-  sha256 ''
+  sha256 :no_check
   
   url "https://raw.githubusercontent.com/TorrenTV/Installers/master/mac/TorrenTV-#{version}-Mac.dmg"
   homepage 'https://torrentv.github.io'
