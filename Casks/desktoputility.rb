a6007d4e278a25cada6bb65bc164b353ad422c44
coeligena <coeligenai@gmail.com>
2014-11-21 03:07:10 +0200

    Create desktoputility.rb

---
+++ Casks/desktoputility.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'desktoputility' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://sweetpproductions.com/products/desktoputility/DesktopUtility.dmg'
+  homepage 'http://sweetpproductions.com/desktoputility/'
+  license :gratis
+
+  app 'DesktopUtility.app'
+end
