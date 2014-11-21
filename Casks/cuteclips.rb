defae7621230c36860cb98aef4c8c7465e88d0dc
coeligena <coeligenai@gmail.com>
2014-11-21 03:00:43 +0200

    Create cuteclips.rb

---
+++ Casks/cuteclips.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'cuteclips' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://cuteclips3.com/CuteClips3.dmg'
+  homepage 'http://cuteclips3.com'
+  license :trial
+
+  app 'CuteClips3.app'
+end
