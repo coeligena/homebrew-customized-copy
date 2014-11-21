db7a55ad67c5074297d84bac03ac6adbe7f94e9b
coeligena <coeligenai@gmail.com>
2014-11-21 03:49:03 +0200

    Update iclip.rb

--- Casks/iclip.rb
+++ Casks/iclip.rb
@@ -2,7 +2,7 @@ cask :v1 => 'iclip' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.irradiatedsoftware.com/downloads/?file=iClip.zip'
+  url 'http://www.irradiatedsoftware.com/download/iClip.zip'
   homepage 'http://www.irradiatedsoftware.com/iclip/'
   license :trial
 
