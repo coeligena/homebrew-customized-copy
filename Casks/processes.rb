a4ea01e6e70d018feed4d13f15f2c6fea9839795
coeligena <coeligenai@gmail.com>
2014-11-21 03:55:44 +0200

    Update processes.rb

--- Casks/processes.rb
+++ Casks/processes.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'processes' do
   version '1.0.1'
-  sha256 ''
+  sha256 '032ae96e845073102dac2183b73e6bb6438a224aa04db51c24676333e6d7d0b2'
 
   url "https://www.oneperiodic.com/files/Processes%20v#{version}.zip"
   homepage 'http://www.oneperiodic.com/products/processes/'
