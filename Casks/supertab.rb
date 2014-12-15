4a8c79b33aeb8c0c6185abd3697f586f00666f26
coeligena <coeligenai@gmail.com>
2014-12-16 01:02:54 +0200

    Update supertab.rb

--- Casks/supertab.rb
+++ Casks/supertab.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'supertab' do
   version '1.2'
-  sha256 'c18d4c4609cc99e976e3d2b32156e12450ca71dc71ec5cba35aa1b8b2137ee70'
+  sha256 '0c6f10647804865b440c9e46ea008b0800e00d6b174a6c488694e1efc01afc18'
 
   url "http://www.supertab.me/SuperTab.dmg"
   homepage 'http://www.supertab.me/'
@@ -8,9 +8,15 @@ cask :v1 => 'supertab' do
 
   pkg 'Double-Click-To-Install.pkg'
 
-  uninstall :pkgutil => "com.spritec.supertab.*",
+  uninstall :quit => 'com.spritec.supertab',
+            :pkgutil => "com.spritec.supertab.*",
             :delete => '/Applications/SuperTab.app'
   zap       :delete => [
+                        '~/Library/Preferences/com.spritec.supertab.decorations',
+                        '~/Library/Preferences/com.spritec.supertab.downloads',
+                        '~/Library/Preferences/com.spritec.supertab.hotkeys',
                         '~/Library/Preferences/com.spritec.supertab.plist',
+                        '~/Library/Preferences/com.spritec.supertab.settings',
+                        '~/Library/Preferences/com.spritec.supertab.state'
                        ]
 end
