6862a26d7d4fdee38e7e67bb6592f0d950cbb5e6
coeligena <coeligenai@gmail.com>
2014-12-21 00:28:41 +0200

    Create epub-mdimporter.rb

---
+++ Casks/epub-mdimporter.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'epub-mdimporter' do
+  version '1.6'
+  sha256 '75a501339f6dc2f8a36efea652c4b7a6e5fd7ceedc36fe50cddb5d5379d7af76'
+
+  url "https://github.com/jaketmp/ePub-quicklook/releases/download/v#{version}/epub.mdimporter.zip"
+  homepage 'https://github.com/jaketmp/ePub-quicklook/releases'
+  license :oss
+
+  preflight do
+    system '/usr/bin/tar', '-xf', '/Library/Caches/Homebrew/epub-mdimporter-1.6.zip'
+    system 'mdimport', '-r', '--', '/Library/Caches/Homebrew/epub.mdimporter'
+  end
+end
