f7a8ba77ffa97efefa4fc8a73a47fc0135369537
coeligena <coeligenai@gmail.com>
2014-12-24 20:08:02 +0200

    Update mps.rb

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -2,7 +2,7 @@ cask :v1 => 'mps' do
   version '3.1.5'
   sha256 '68dd55b0542ddb82fe941d465f41b035730c77849c6b13b93f07e5d7091bfb8c'
 
-  url "http://download.jetbrains.com/mps/#{version.sub(%r{^(\d+\.\d).*},'\1\2')}/MPS-#{version}-macos.dmg"
+  url "http://download.jetbrains.com/mps/#{version.sub(%r{^(\d+)\.(\d).*},'\1\2')}/MPS-#{version}-macos.dmg"
   homepage 'https://www.jetbrains.com/mps/'
   license :oss
 
@@ -13,10 +13,10 @@ cask :v1 => 'mps' do
   end
 
   zap :delete => [
-                  "~/Library/Application Support/MPS#{version.sub(%r{^(\d+\.\d).*},'\1\2')}",
-                  "~/Library/Preferences/MPS#{version.sub(%r{^(\d+\.\d).*},'\1\2')}",
-                  "~/Library/Caches/MPS#{version.sub(%r{^(\d+\.\d).*},'\1\2')}",
-                  "~/Library/Logs/MPS#{version.sub(%r{^(\d+\.\d).*},'\1\2')}",
+                  "~/Library/Application Support/MPS#{version.sub(%r{^(\d+)\.(\d).*},'\1\2')}",
+                  "~/Library/Preferences/MPS#{version.sub(%r{^(\d+)\.(\d).*},'\1\2')}",
+                  "~/Library/Caches/MPS#{version.sub(%r{^(\d+)\.(\d).*},'\1\2')}",
+                  "~/Library/Logs/MPS#{version.sub(%r{^(\d+)\.(\d).*},'\1\2')}",
                  ]
 
   caveats <<-EOS.undent
