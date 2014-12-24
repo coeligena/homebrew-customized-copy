18a36f58b871b4cf6fd10fe48e38772497c80d72
coeligena <coeligenai@gmail.com>
2014-12-24 14:14:32 +0200

    Update herald.rb

--- Casks/herald.rb
+++ Casks/herald.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'herald' do
   version '5.0.1'
-  sha256 ''
+  sha256 'fc6dc1e978e7a6b5529254c16231e442e9f3790d53e0b0ceaacdb0bad124ed8a'
 
   url "http://erikhinterbichler.com/blog/wp-content/uploads/software/Herald.zip?v=#{version}"
   homepage 'http://erikhinterbichler.com/apps/herald/'
