0c25955cd309761df21efa6d499107cbdf8edaf5
coeligena <coeligenai@gmail.com>
2014-12-25 15:42:34 +0200

    Update dealalert.rb

--- Casks/dealalert.rb
+++ Casks/dealalert.rb
@@ -2,7 +2,7 @@ cask :v1 => 'dealalert' do
   version :latest
   sha256 :no_check
 
-  url 'http://littlefin.com/downloads/dealalert.zip'
+  url 'http://cl.ly/3G0n0R1t330U/download/DealAlert.app.zip'
   appcast 'http://www.littlefin.com/downloads/dealalert.xml'
   homepage 'http://dealalertapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
