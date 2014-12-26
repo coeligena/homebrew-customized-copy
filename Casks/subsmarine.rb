8e781d3928c6b7a04a8aca8a74b91dfee05da13f
coeligena <coeligenai@gmail.com>
2014-12-26 15:33:10 +0200

    Update subsmarine.rb

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -2,7 +2,7 @@ cask :v1 => 'subsmarine' do
   version '1.2.1'
   sha256 :no_check
 
-  url "http://www.cocoawithchurros.com/downloads/SubsMarine.app.#{version}.zip"
+  url "http://www.cocoawithchurros.com/downloads/SubsMarine.#{version}.zip"
   appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7',
           :sha256 => '9659830b134ac12326639e1b3f7ba06945cc18db343890e06a4c7f9d9dce8912'
   homepage 'http://www.cocoawithchurros.com/subsmarine.php'
