8a589b2bcac59618efb5d70366c53e9ba016ddf7
coeligena <coeligenai@gmail.com>
2014-12-30 14:02:27 +0200

    Create swift-dictation.rb

---
+++ Casks/swift-dictation.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'swift-dictation' do
+  version :latest
+  sha256 :no_check
+
+  url "http://cl.ly/Vwxq/download/Swift%20Dictation.zip"
+  homepage 'https://ntk.me/2012/12/17/dictation/'
+  license :gratis
+
+  app 'Swift Dictation.app'
+end
