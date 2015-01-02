80edf2c3b9036970e320a35c5cc92b0c6447f44d
coeligena <coeligenai@gmail.com>
2015-01-03 00:10:12 +0200

    Update cookie-stumbler.rb

--- Casks/cookie-stumbler.rb
+++ Casks/cookie-stumbler.rb
@@ -5,7 +5,7 @@ cask :v1 => 'cookie-stumbler' do
   url 'https://cookiestumbler.s3.amazonaws.com/Cookie%20Stumbler.dmg'
   appcast 'http://www.cookiestumbler.com/Software%20Updates/Cookie%20Stumbler.xml'
   homepage 'http://www.writeitstudios.com/cookiestumbler.php'
-  license :trial
+  license :commercial
   
   app 'Cookie Stumbler.app'
 end
