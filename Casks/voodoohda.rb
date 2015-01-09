90fa781b105e3c45b3e19808a514018382dbaa4f
coeligena <coeligenai@gmail.com>
2015-01-09 23:02:53 +0200

    Create voodoohda.rb

---
+++ Casks/voodoohda.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'voodoohda' do
+  version '2.8.7'
+  sha256 'c42a9517f26b1096448b5103db4ceead1d8a73b9b9279b4673638d97393dfc7f'
+  
+  url "http://www.hackintoshosx.com/files/download/1194-voodoohda-287-pkg-installer/"
+  homepage '"http://www.hackintoshosx.com/files/download/1194-voodoohda-287-pkg-installer/'
+  license :oss
+  
+  pkg 'VoodooHDA-v2.8.7.pkg'
+end
