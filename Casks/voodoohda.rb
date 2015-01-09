ec7a0cb6ad2d5068ddee3cb9d285e97cecad1da9
coeligena <coeligenai@gmail.com>
2015-01-09 23:18:52 +0200

    Update voodoohda.rb

--- Casks/voodoohda.rb
+++ Casks/voodoohda.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'voodoohda' do
   version '2.8.8'
-  sha256 'c42a9517f26b1096448b5103db4ceead1d8a73b9b9279b4673638d97393dfc7f'
+  sha256 '9df9ac1c861e8126dbb8ec4246261db85f484047f60d2a7b33e18f996b37326d'
   
   url "http://www.hackintoshosx.com/files/getdownload/12972-voodoohda/"
   homepage 'http://www.hackintoshosx.com/tags/downloads/RealtekHDA/'
