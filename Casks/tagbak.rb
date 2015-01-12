14f41a2cfd7d449ebf124381433490d1b1f07565
coeligena <coeligenai@gmail.com>
2015-01-13 00:28:42 +0200

    Update tagbak.rb

--- Casks/tagbak.rb
+++ Casks/tagbak.rb
@@ -2,7 +2,7 @@ cask :v1 => 'tagbak' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/ttscoff/tagbak/zipball/master'
+  url 'https://github.com/ttscoff/tagbak/archive/master.zip'
   homepage 'http://brettterpstra.com/projects/tagbak/'
   license :oss
 
