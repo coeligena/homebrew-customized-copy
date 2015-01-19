d05b4322688bceaeeb4b16a8f1f0a45082993ccf
coeligena <coeligenai@gmail.com>
2015-01-20 00:35:23 +0200

    Update prizmo.rb

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'prizmo' do
-  version '3.0.1'
+  version '3.1'
   sha256 '270077128ebeff1f555362254a72a14e6f9ef060316b5a2d4faed642e2362b58'
   
-  url 'http://www.creaceed.com/downloads/prizmo3.zip'
+  url 'http://www.creaceed.com/downloads/prizmo3_#{version}.zip'
   appcast 'http://www.creaceed.com/appcasts/prizmo3.xml'
   homepage 'http://www.creaceed.com/prizmo'
 
