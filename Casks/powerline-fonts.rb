cb0fe2c55e7f7e0e70e1cfa61967ea2f2bb0c65e
coeligena <coeligenai@gmail.com>
2015-01-19 23:20:36 +0200

    Create powerline-fonts.rb

---
+++ Casks/powerline-fonts.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'powerline-fonts' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/powerline/fonts/archive/master.zip'
+  homepage 'https://github.com/powerline/fonts/'
+  license :oss
+  
+  installer :script => 'install.sh',
+            :sudo => false
+end
