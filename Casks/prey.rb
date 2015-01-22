5cd30c390f8e084c99f61f5bf10c71ca69666f12
coeligena <coeligenai@gmail.com>
2015-01-22 22:47:58 +0200

    Update prey.rb

--- Casks/prey.rb
+++ Casks/prey.rb
@@ -6,7 +6,7 @@ cask :v1 => 'prey' do
   homepage 'https://preyproject.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg "prey-mac-#{version}-x64"
+  pkg "prey-mac-#{version}-x64.pkg"
 
   uninstall :pkgutil => 'com.forkhq.prey'
   caveats <<-EOS.undent
