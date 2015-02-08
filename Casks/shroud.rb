63f03ec0b91d33d15201be93f0c71587bb2a1272
Coel <coeligenai@gmail.com>
2015-02-08 12:55:26 +0200

    New - shroud

---
+++ Casks/shroud.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'shroud' do
+  version '1.2'
+  sha256 'fb1fc2cad188131714ea3505fa65f20f8d79da758dc86b2dc2ed9073648900af'
+
+  url "http://sabi.net/nriley/software/Shroud-#{version}.dmg"
+  name 'Shroud'
+  homepage 'http://sabi.net/nriley/software/index.html#shroud'
+  license :oss
+
+  app 'Shroud.app'
+end
