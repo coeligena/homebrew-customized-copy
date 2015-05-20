f7b9e1a0a51459c19a6e287a04d564692efd0562
Coel <coeligenai@gmail.com>
2015-05-20 13:33:55 +0300

    no message

--- Casks/devonthink-personal.rb
+++ Casks/devonthink-personal.rb
@@ -1,12 +1,14 @@
 cask :v1 => 'devonthink-personal' do
-    version '2.8.1'
-    sha256 'e1831060b011826670713e837cd557038dba308cb8b02a15e272adc713fa03c0'
+    version '2.8.4'
+    sha256 'd17884f211aa4129dcf95c75f1c5f306ff405fda560dad0af991880b96edccbd'
 
     url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Personal.app.zip"
     appcast 'http://www.devon-technologies.com/Sparkle/DEVONthink2.xml',
             :sha256 => '5166d03885dc47d5d4fe1aadd1f782d346fcc7a5f04a855657490b05d2559c92'
     homepage 'http://www.devontechnologies.com/products/devonthink/devonthink-personal.html'
     license :commercial
+    
+    container :nested => 'DEVONthink_Personal.dmg'
 
     app 'DEVONthink.app'
 end
