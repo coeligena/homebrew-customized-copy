1105b80f0c34b5f8fdf352af089180973241b8fd
Coel <coeligenai@gmail.com>
2015-07-23 14:27:20 +0300

    no message

---
+++ Casks/maccleanse.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'maccleanse' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.koingosw.com/products/getmirrorfile.php?path=%2Fproducts%2Fmaccleanse%2Fdownload%2Fmaccleanse.dmg'
+  name 'MacCleanse'
+  homepage 'https://www.koingosw.com/products/maccleanse/'
+  license :commercial
+
+  app 'MacCleanse.app'
+end
