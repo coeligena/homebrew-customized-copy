7263724120b967cc913d37af2bf8c2c48a8b57cf
Coel <coeligenai@gmail.com>
2015-09-01 22:58:57 +0300

    no message

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'expandrive' do
-  version '5.0.11'
-  sha256 '39cbdf4a8a5f6f9280779a0181870f1b459be4ce6cac450d2707cb978dc0019e'
+  version :latest
+  sha256 :no_check
 
-  url "http://updates.expandrive.com/apps/expandrive/v/#{version.gsub('.','-')}/download.dmg"
+  url "http://updates.expandrive.com/apps/expandrive/download_latest"
   name 'ExpanDrive'
   appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3',
           :sha256 => 'bb97d4e362c75588d0c2e67d3cfdbd8e3c6200ebd524325b5f8c81560240ca5c'
