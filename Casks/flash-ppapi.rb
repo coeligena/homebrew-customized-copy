836ad3845b63ab7e1c25ade06dde95a521c55855
Coel <coeligenai@gmail.com>
2015-12-09 01:12:35 +0200

    no message

--- Casks/flash-ppapi.rb
+++ Casks/flash-ppapi.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash-ppapi' do
-  version '19.0.0.226'
-  sha256 '7c9a7021f5aeaa062c6349c60c81f89dd46087463522ce931f5d4b859f687be5'
+  version '20.0.0.228'
+  sha256 '3281a2b980a8509c2684092522f4a56c26e78150a7eb23d2648137a92f5ebe1c'
 
   # macromedia.com is the official download host per the vendor homepage
   url "https://fpdownload.macromedia.com/get/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
